2e007687b2af721922e5637aec1943af063eadd6
Tadeo <tadeoriverosk@gmail.com>
2014-07-02 20:55:42 -0300

    Merge origin/master
    Conflicts:
    	app/scripts/controllers/main.js
    	app/styles/main.css
    	app/views/main.html

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'univ',
+			'username'  => 'root',
+			'password'  => 'Caterpillar4',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
